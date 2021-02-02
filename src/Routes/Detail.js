192e66e71ab7b735d2e73911ad2f7800489bc8ea
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-02 22:53:44 +0900

    Enter a character to render on the screen

--- src/Routes/Detail.js
+++ src/Routes/Detail.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Detail = () => {
-  return <div></div>;
+  return <div>Detail</div>;
 };
 
 export default Detail;
--- src/Routes/TV.js
+++ src/Routes/TV.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TV = () => {
-  return <div></div>;
+  return <div>TV</div>;
 };
 
 export default TV;
