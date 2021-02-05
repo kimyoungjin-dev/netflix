b32c48d280f7df2ddebb6409893b6e5ab880c30a
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-06 01:12:00 +0900

    Get the section and play it now and show you popular and coming on screen

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,7 +1,31 @@
 import PropTypes from "prop-types";
+import styled from "styled-components";
+import Section from "Components/Section";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
 
 const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
-  null;
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Play">
+          {nowPlaying.map((movie) => movie.title)}
+        </Section>
+      )}
+
+      {upcoming && upcoming.length > 0 && (
+        <Section title="UpComing">
+          {upcoming.map((movie) => movie.title)}
+        </Section>
+      )}
+
+      {popular && popular.length > 0 && (
+        <Section title="Popular">{popular.map((movie) => movie.title)}</Section>
+      )}
+    </Container>
+  );
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
