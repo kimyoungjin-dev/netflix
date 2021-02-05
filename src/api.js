d8eabeff2529afbe6d9cbc5987e80f4a48e7fc03
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-05 23:15:44 +0900

    Create api.js
    Get api key and information from https://api.themoviedb.org/3/

---
+++ src/api.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "84746f07785a093ed42c3cee12d5a642",
+    language: "en-US",
+  },
+});
+
+export const movieApi = {
+  upcoming: () => api.get("movie/upcoming"),
+  nowPlaying: () => api.get("movie/now_playing"),
+  popular: () => api.get("movie/popular"),
+  search: (term) =>
+    api.get("search/company", {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
+  movieDetail: () =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  airingToday: () => api.get("tv/airing_today"),
+  popular: () => api.get("tv/popular"),
+  search: (term) =>
+    api.get("search/company", {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
+  tvDetail: () =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+};
