74466b9402998246607ce34c62c8962f950e4804
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-07 02:32:53 +0900

    display tv information on the screen

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,7 +1,37 @@
+import Section from "Components/Section";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Container = styled.div``;
 
 const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
-  null;
+  loading ? null : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="TopRated">
+          {topRated.map((show) => (
+            <span>{show.name}</span>
+          ))}
+        </Section>
+      )}
+
+      {popular && popular.length > 0 && (
+        <Section title="Popular">
+          {popular.map((show) => (
+            <span>{show.name}</span>
+          ))}
+        </Section>
+      )}
+
+      {airingToday && airingToday.length > 0 && (
+        <Section title="AiringToday">
+          {airingToday.map((show) => (
+            <span>{show.name}</span>
+          ))}
+        </Section>
+      )}
+    </Container>
+  );
 
 TVPresenter.propTypes = {
   topRated: PropTypes.array,
