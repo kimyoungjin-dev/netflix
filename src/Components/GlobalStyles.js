1c7c78a616120c5f7f3e966dc8a90003363db724
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-08 04:05:52 +0900

    Change padding value in globalstyles.js

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -18,8 +18,8 @@ const globalStyles = createGlobalStyle`
         font-size:12px;
         background-color:rgba(20, 20, 20, 1);
         color:white;
-        padding-top:80px;
-
+        padding-left:10px;
+        padding-top:90px;
     }
 `;
 
