53082d78e26dd70a710ebe79d1539e09d00aa048
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-17 02:54:31 +0900

    Design CSS

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -5,7 +5,7 @@ import Loader from "Components/Loader";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import Massage from "Components/Massage";
 import YoutubeIcon from "images/YoutubeImage.jpeg";
-import { AiFillStar } from "react-icons/ai";
+import { AiFillStar, AiOutlineConsoleSql } from "react-icons/ai";
 
 const Container = styled.div`
   position: relative;
@@ -42,8 +42,7 @@ const Cover = styled.div`
   background-image: url(${(props) => props.bgImage});
   background-position: center center;
   background-size: cover;
-  height: 86%;
-  margin-right: 30px;
+  height: 56%;
   border-radius: 5px;
 `;
 
@@ -53,26 +52,11 @@ const Data = styled.div`
   width: 70%;
 `;
 
-const TitleContainer = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-`;
-
 const Title = styled.h3`
   font-size: 35px;
   margin-bottom: 20px;
 `;
 
-const Seasons = styled.span`
-  font-size: 17px;
-  margin-left: 10px;
-`;
-
-const SeasonName = styled.span`
-  font-size: 12px;
-  margin: 0px 4px;
-`;
 const ItemContainer = styled.div`
   margin-top: 15px;
   margin-bottom: 10px;
@@ -128,29 +112,36 @@ const OverView = styled.p`
 
 const ItemTitle = styled.span`
   color: white;
-  font-size: 26px;
+  font-size: 17px;
   margin-right: 10px;
 `;
 
+const MoviMakerYoutubeContainer = styled.div`
+  height: 22vh;
+  margin: 8px 0px;
+  width: 100%;
+`;
+
+const MovieMakerYoutubeList = styled.div`
+  display: flex;
+`;
+
 const MovieMaker = styled.div`
-  margin: 30px 0px;
   display: flex;
   flex-direction: column;
   color: white;
   opacity: 0.6;
-  justify-content: space-between;
-  height: 15%;
   font-style: oblique;
+  justify-content: space-between;
+  width: 90%;
 `;
 
-//YouTubeContainer
 const YouTubeContainer = styled.div`
-  flex-direction: column;
   width: 100%;
-  height: 20vh;
+  height: 100%;
   display: flex;
+  flex-direction: column;
   position: relative;
-  justify-content: space-between;
   cursor: pointer;
   & :last-child {
   }
@@ -159,36 +150,67 @@ const YouTubeContainer = styled.div`
 const YoutubeContents = styled.a`
   display: flex;
   align-items: center;
-  margin-top: 40px;
+  width: 100%;
+  height: 11vh;
+  justify-content: center;
 `;
 
 const YoutubeImage = styled.div`
-  border-radius: 50%;
-  height: 110px;
-  width: 110px;
+  height: 60px;
+  width: 60px;
   background-image: url(${(props) => props.bgImage});
   background-position: center center;
   background-size: cover;
-  background-color: blue;
-`;
-
-const YoutubeLinkAdress = styled.div`
-  margin-left: 20px;
-  text-decoration: underline;
-  opacity: 0.6;
-  font-size: 18px;
+  border-radius: 4px;
+  margin-right: 13px;
+  border-radius: 100%;
 `;
 
 const YouTubeIconImage = styled.img`
+  width: 30px;
+  height: 30px;
   position: absolute;
-  width: 35px;
-  height: 35px;
-  top: 30px;
-  left: -10px;
-  border-radius: 40%;
+  left: 65px;
+  top: -10px;
+  border-radius: 100%;
 `;
 
-//Components
+const SeasonsContainer = styled.div`
+  margin-top: 30px;
+  width: 100%;
+  height: 13.5vh;
+  font-size: 14px;
+`;
+
+const SeasonBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const SeasonList = styled.div`
+  :not(:first-child) {
+    margin-left: 35px;
+  }
+`;
+const SeasonName = styled.div`
+  font-weight: 600;
+  margin: 5px 0px;
+`;
+
+const SeasonYear = styled.div`
+  opacity: 0.6;
+`;
+
+const SeasonImage = styled.img`
+  height: 60px;
+  width: 120px;
+  border-radius: 5px;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+`;
+
 const DetailPresenter = ({ result, loading, error }) => {
   return (
     <HelmetProvider>
@@ -217,26 +239,11 @@ const DetailPresenter = ({ result, loading, error }) => {
               bgImage={`https://image.tmdb.org/t/p/original${result.poster_path}`}
             />
             <Data>
-              <TitleContainer>
-                <Title>
-                  {result.original_title
-                    ? result.original_title
-                    : result.original_name}
-                </Title>
-
-                <Seasons>
-                  {result.seasons &&
-                    result.seasons.map((season, index) => (
-                      <SeasonName key={index}>
-                        {`${
-                          index !== result.seasons.length - 1
-                            ? `${season.name}`
-                            : ` / ${season.name}`
-                        }`}
-                      </SeasonName>
-                    ))}
-                </Seasons>
-              </TitleContainer>
+              <Title>
+                {result.original_title
+                  ? result.original_title
+                  : result.original_name}
+              </Title>
 
               <ItemContainer>
                 <Item>{`${
@@ -274,78 +281,94 @@ const DetailPresenter = ({ result, loading, error }) => {
                 </Popularity>
               </ItemContainer>
 
-              <OverView>{result.overview}..</OverView>
-              <MovieMaker>
-                <Item>
-                  <ItemTitle>Production Company</ItemTitle>
-                  {result.production_companies[0] &&
-                    result.production_companies[0].name}
-                </Item>
+              <OverView>
+                {result.overview && result.overview.substring(0, 330)}..
+              </OverView>
 
-                <Item>
-                  <ItemTitle>Production_countries</ItemTitle>
-                  {result.production_countries[0] &&
-                    result.production_countries[0].name}
-                </Item>
-                <Item>
-                  <ItemTitle>Genres</ItemTitle>
-                  {result.genres &&
-                    result.genres.map((genre, index) =>
-                      index === result.genres.length - 1
-                        ? genre.name
-                        : `${genre.name} / `
-                    )}
-                </Item>
-              </MovieMaker>
-              <YouTubeContainer>
-                <YoutubeContents
-                  href={`https://youtube.com/watch?v=${
-                    result.videos.results[0] && result.videos.results[0].key
-                  }`}
-                  target="_blank"
-                >
-                  <YoutubeImage
-                    bgImage={
-                      result.poster_path && result.poster_path
-                        ? `https://image.tmdb.org/t/p/original${result.poster_path}`
-                        : require("images/YoutubeImage.jpeg")
-                    }
-                  />
+              <MoviMakerYoutubeContainer>
+                <MovieMakerYoutubeList>
+                  <MovieMaker>
+                    <Item>
+                      <ItemTitle>Production Company</ItemTitle>
+                      {result.production_companies[0] &&
+                        result.production_companies[0].name}
+                    </Item>
 
-                  <YoutubeLinkAdress>
-                    {`https://www.youtube.com/watch?v=${
-                      result.videos.results[0] && result.videos.results[0].key
-                    }`}
-                  </YoutubeLinkAdress>
-                  <YouTubeIconImage src={YoutubeIcon} />
-                </YoutubeContents>
+                    <Item>
+                      <ItemTitle>Production_countries</ItemTitle>
+                      {result.production_countries[0] &&
+                        result.production_countries[0].name}
+                    </Item>
+                    <Item>
+                      <ItemTitle>Genres</ItemTitle>
+                      {result.genres &&
+                        result.genres.map((genre, index) =>
+                          index === result.genres.length - 1
+                            ? genre.name
+                            : `${genre.name} / `
+                        )}
+                    </Item>
+                  </MovieMaker>
 
-                <YoutubeContents
-                  href={`https://youtube.com/watch?v=${
-                    result.videos.results[0] && result.videos.results[0].key
-                  }`}
-                  target="_blank"
-                >
-                  <YoutubeImage
-                    bgImage={
-                      result && result.backdrop_path && result.backdrop_path
-                        ? `https://image.tmdb.org/t/p/original${
-                            result.backdrop_path && result.backdrop_path
-                          }`
-                        : ` https://image.tmdb.org/t/p/original${
-                            result.poster_path && result.poster_path
-                          }`
-                    }
-                  />
-                  <YoutubeLinkAdress>{`https://www.youtube.com/watch?v=${
-                    result.videos.results[1] && result.videos.results[1].key
-                      ? result.videos.results[1].key
-                      : result.videos.results[0]
-                      ? result.videos.results[0].key
-                      : ""
-                  }`}</YoutubeLinkAdress>
-                </YoutubeContents>
-              </YouTubeContainer>
+                  <YouTubeContainer>
+                    <YoutubeContents
+                      href={`https://youtube.com/watch?v=${
+                        result.videos.results[0] && result.videos.results[0].key
+                      }`}
+                      target="_blank"
+                    >
+                      <YoutubeImage
+                        bgImage={
+                          result.poster_path && result.poster_path
+                            ? `https://image.tmdb.org/t/p/original${result.poster_path}`
+                            : require("images/YoutubeImage.jpeg")
+                        }
+                      />
+                      <YouTubeIconImage src={YoutubeIcon} />
+                    </YoutubeContents>
+
+                    <YoutubeContents
+                      href={`https://youtube.com/watch?v=${
+                        result.videos.results[0] && result.videos.results[0].key
+                      }`}
+                      target="_blank"
+                    >
+                      <YoutubeImage
+                        bgImage={
+                          result && result.backdrop_path && result.backdrop_path
+                            ? `https://image.tmdb.org/t/p/original${
+                                result.backdrop_path && result.backdrop_path
+                              }`
+                            : ` https://image.tmdb.org/t/p/original${
+                                result.poster_path && result.poster_path
+                              }`
+                        }
+                      />
+                    </YoutubeContents>
+                  </YouTubeContainer>
+                </MovieMakerYoutubeList>
+              </MoviMakerYoutubeContainer>
+
+              <SeasonsContainer>
+                <SeasonBox>
+                  {result.seasons &&
+                    result.seasons.map((season) => (
+                      <>
+                        <SeasonList>
+                          <SeasonImage
+                            bgImage={`https://image.tmdb.org/t/p/original${
+                              season.poster_path && season.poster_path
+                            }`}
+                          />
+                          <SeasonName>{season.name && season.name}</SeasonName>
+                          <SeasonYear>
+                            {season.air_date && season.air_date.substring(0, 4)}
+                          </SeasonYear>
+                        </SeasonList>
+                      </>
+                    ))}
+                </SeasonBox>
+              </SeasonsContainer>
             </Data>
           </Content>
           {error && <Massage color="red" text={error} />}
