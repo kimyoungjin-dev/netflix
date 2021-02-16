3c02938961dc803363c59aa4029dfcebb8c10b44
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-16 20:30:00 +0900

    Delete unused useefftect

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,5 @@
 import { movieApi } from "api";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import SearchPresenter from "./SearchPresenter";
 
 const SearchContainer = () => {
