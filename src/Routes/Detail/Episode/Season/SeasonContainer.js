229a257bc39fdb5465d3241ce6d577056a5ec5e6
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-19 15:51:27 +0900

    Create SeasonContainer.js

---
+++ src/Routes/Detail/Episode/Season/SeasonContainer.js
@@ -0,0 +1,53 @@
+import { movieApi, tvApi } from "api";
+import React, { useEffect, useState } from "react";
+import SeasonPresenter from "./SeasonPresenter";
+
+const SeasonContainer = (props) => {
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getDetail = async () => {
+    const {
+      history: { push },
+      match: {
+        params: { id },
+      },
+    } = props;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+
+    const {
+      location: { pathname },
+    } = props;
+    const isMovie = pathname.includes("movie");
+
+    try {
+      if (isMovie) {
+        const { data: result } = await movieApi.movieDetail(id);
+        setResult(result);
+      } else {
+        const { data: result } = await tvApi.showDetail(id);
+        setResult(result);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getDetail();
+  }, []);
+
+  return (
+    <>
+      <SeasonPresenter result={result} loading={loading} error={error} />
+    </>
+  );
+};
+
+export default SeasonContainer;
