7ce6fb7f02ea068c92af997963f1359764b0f725
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-19 15:51:48 +0900

    Create HelpContainer

---
+++ src/Routes/Detail/Help/HelpContainer.js
@@ -0,0 +1,56 @@
+import { movieApi, tvApi } from "api";
+import React, { useEffect, useState } from "react";
+import HelpPresenter from "./HelpPresenter";
+
+const HelpContainer = (props) => {
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [editing, setEditing] = useState(false);
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+
+  const getDetail = async () => {
+    const {
+      history: { push },
+      match: {
+        params: { id },
+      },
+    } = props;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+
+    const {
+      location: { pathname },
+    } = props;
+    const isMovie = pathname.includes("movie");
+
+    try {
+      if (isMovie) {
+        const { data: result } = await movieApi.movieDetail(id);
+        setResult(result);
+      } else {
+        const { data: result } = await tvApi.showDetail(id);
+        setResult(result);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getDetail();
+  }, []);
+
+  return (
+    <>
+      <HelpPresenter result={result} loading={loading} error={error} />
+    </>
+  );
+};
+
+export default HelpContainer;
