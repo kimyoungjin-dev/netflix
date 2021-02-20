01971b4c11d3409e2e094c8ea29b03f2b8d14d2d
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-20 20:41:28 +0900

    Modify Import

--- src/Routes/Detail/DetailContents.js
+++ src/Routes/Detail/DetailContents.js
@@ -3,15 +3,15 @@ import React from "react";
 import styled from "styled-components";
 import { AiFillStar } from "react-icons/ai";
 import { Link, Route, withRouter } from "react-router-dom";
-import Overview from "./Overview";
-import MovieEpisode from "./MovieEpisode";
-import Episode from "./Episode";
-import Help from "./Help";
+import Overview from "Components/Overview";
+import MovieEpisode from "Components/MovieEpisode";
+import ShowEpisode from "Components/ShowEpisode";
+import Help from "Components/Help";
+import Creadit from "Components/Creadit";
+import Review from "Components/Review";
 
 const Container = styled.div`
   position: relative;
-  height: 100vh;
-  width: 100%;
 `;
 
 const NetflexOriginal = styled.div`
@@ -26,21 +26,20 @@ const Netflex = styled.div`
 const Original = styled.div`
   font-size: 70px;
   opacity: 0.6;
-  margin-left: 10px;
+  margin-left: 15px;
 `;
 
 const Title = styled.div`
   font-family: "Reggae One", cursive;
   font-size: 60px;
-  margin-top: 20px;
-  margin-bottom: 40px;
+  margin: 40px 0px;
   font-weight: 600;
 `;
 
 const VoteContainer = styled.div`
   position: absolute;
   right: 30px;
-  top: px;
+  top: 120px;
   display: flex;
   font-size: 55px;
 `;
@@ -57,143 +56,164 @@ const ItemContainer = styled.div`
   font-size: 20px;
   margin-right: 10px;
   display: flex;
+  margin-bottom: 10px;
+  width: 100%;
 `;
 
 const Item = styled.div`
-  margin-right: 10px;
-  margin-bottom: 10px;
+  margin-right: 20px;
+  & span {
+    margin-right: 10px;
+  }
 `;
 
+const RuntimeBtn = styled.button`
+  all: unset;
+  padding: 3px;
+  background: #c6ffdd;
+  background: -webkit-linear-gradient(to right, #f7797d, #fbd786, #c6ffdd);
+  background: linear-gradient(to right, #f7797d, #fbd786, #c6ffdd);
+  color: black;
+  border-radius: 10px;
+`;
 const Tab = styled.div`
   display: flex;
-  opacity: 0.8;
-  padding: 5px;
 `;
 
 const TabTitle = styled.h2`
-  opacity: 0.7;
   margin-right: 40px;
   font-size: 30px;
   font-family: "Nunito", sans-serif;
-  margin-bottom: 10px;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.4);
+  &:hover {
+    border-bottom: 2px solid red;
+  }
 `;
 
-const DetailContents = withRouter(
-  ({ location: { pathname }, result, loading, error }) => {
-    const isMovie = pathname.includes("movie");
+const DetailContents = ({
+  props: {
+    location: { pathname },
+  },
+  result,
+  loading,
+  error,
+}) => {
+  const isMovie = pathname.includes("movie");
 
-    return (
-      <>
-        {loading ? (
-          <Loader />
-        ) : (
-          <Container>
-            <NetflexOriginal>
-              <Netflex>Netflex</Netflex>
-              <Original>Original</Original>
-            </NetflexOriginal>
-            <Title>
-              {result && result.original_name
-                ? result.original_name
-                : result.original_title}
-            </Title>
-            <VoteContainer>
-              <Vote> {result.vote_average} </Vote>
-              <VoteIcon>
-                <AiFillStar />
-              </VoteIcon>
-            </VoteContainer>
-            <ItemContainer>
-              <Item>
-                {`${
-                  result.first_air_date
-                    ? result.first_air_date.substring(0, 4)
-                    : result.release_date && result.release_date.substring(0, 4)
-                } |
-              ${
-                result.seasons
-                  ? result.seasons.map((season) => season.name)
-                  : "First Season"
-              } |
-              ${
-                result.episode_run_time && result.episode_run_time
-                  ? result.episode_run_time
-                  : result.runtime && result.runtime
-              }`}
-              </Item>
-            </ItemContainer>
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Container>
+          <NetflexOriginal>
+            <Netflex>Netflex</Netflex>
+            <Original>Original</Original>
+          </NetflexOriginal>
+          <Title>
+            {result && result.original_name
+              ? result.original_name
+              : result.original_title}
+          </Title>
+          <VoteContainer>
+            <Vote> {result.vote_average} </Vote>
+            <VoteIcon>
+              <AiFillStar />
+            </VoteIcon>
+          </VoteContainer>
+          <ItemContainer>
+            <Item>
+              {result.first_air_date
+                ? result.first_air_date.substring(0, 4)
+                : result.release_date && result.release_date.substring(0, 4)}
+            </Item>
+            <Item>
+              {result.seasons
+                ? result.seasons.map((season) => <span>{season.name}</span>)
+                : "First Season"}
+            </Item>
+            <RuntimeBtn>
+              {result.episode_run_time && result.episode_run_time
+                ? result.episode_run_time
+                : result.runtime && result.runtime}
+              minutes
+            </RuntimeBtn>
+          </ItemContainer>
 
-            <Tab>
-              <Link
-                to={
-                  isMovie
-                    ? `/movie/${result.id}/overview`
-                    : `/show/${result.id}/overview`
-                }
-              >
-                <TabTitle
-                  current={
-                    pathname === isMovie
-                      ? `/movie/:id/overview`
-                      : `/show/:id/overview`
-                  }
-                >
-                  Overview
-                </TabTitle>
-              </Link>
+          <Tab>
+            <Link
+              to={
+                isMovie
+                  ? `/movie/${result.id}/overview`
+                  : `/show/${result.id}/overview`
+              }
+            >
+              <TabTitle>Overview</TabTitle>
+            </Link>
+            <Link
+              to={
+                isMovie
+                  ? `/movie/${result.id}/movieepisode`
+                  : `/show/${result.id}/episode`
+              }
+            >
+              <TabTitle>{isMovie ? "MovieEpisode" : "ShowEpisode"}</TabTitle>
+            </Link>
+            <Link
+              to={
+                isMovie ? `/movie/${result.id}/help` : `/show/${result.id}/help`
+              }
+            >
+              <TabTitle>Help</TabTitle>
+            </Link>
 
-              <Link
-                to={
-                  isMovie
-                    ? `/movie/${result.id}/movieepisode`
-                    : `/show/${result.id}/episode`
-                }
-              >
-                <TabTitle
-                  current={
-                    isMovie ? `/movie/:id/movieepisode` : `/show/:id/episode`
-                  }
-                >
-                  {isMovie ? "MovieEpisode" : "ShowEpisode"}
-                </TabTitle>
-              </Link>
+            <Link
+              to={
+                isMovie
+                  ? `/movie/${result.id}/creadit`
+                  : `/show/${result.id}/creadit`
+              }
+            >
+              <TabTitle>Creadit</TabTitle>
+            </Link>
 
-              <Link
-                to={
-                  isMovie
-                    ? `/movie/${result.id}/help`
-                    : `/show/${result.id}/help`
-                }
-              >
-                <TabTitle
-                  current={
-                    pathname === isMovie ? `/movie/:id/help` : `/show/:id/help`
-                  }
-                >
-                  Help
-                </TabTitle>
-              </Link>
-            </Tab>
+            <Link
+              to={
+                isMovie
+                  ? `/movie/${result.id}/review`
+                  : `/show/${result.id}/review`
+              }
+            >
+              <TabTitle>Review</TabTitle>
+            </Link>
+          </Tab>
 
-            <Route
-              path={isMovie ? `/movie/:id/overview` : `/show/:id/overview`} //overview
-              component={Overview}
-            />
+          <Route
+            path={isMovie ? `/movie/:id/overview` : `/show/:id/overview`} //overview
+            component={Overview}
+          />
 
-            <Route path="/movie/:id/movieepisode" component={MovieEpisode} />
+          <Route path="/movie/:id/movieepisode" component={MovieEpisode} />
 
-            <Route path="/show/:id/episode" component={Episode} />
+          <Route path="/show/:id/episode" component={ShowEpisode} />
 
-            <Route
-              path={isMovie ? `/movie/:id/help` : `/show/:id/help`}
-              component={Help}
-            />
-          </Container>
-        )}
-      </>
-    );
-  }
-);
+          <Route
+            path={isMovie ? `/movie/:id/help` : `/show/:id/help`}
+            component={Help}
+          />
+
+          <Route
+            path={isMovie ? `/movie/:id/creadit` : `/show/:id/creadit`}
+            component={Creadit}
+          />
+
+          <Route
+            path={isMovie ? `/movie/:id/review` : `/show/:id/review`}
+            component={Review}
+          />
+        </Container>
+      )}
+    </>
+  );
+};
 
 export default DetailContents;
