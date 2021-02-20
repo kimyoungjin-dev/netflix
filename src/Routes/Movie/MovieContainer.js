1c838338e3baf82365b643ed06f3e098a7342e16
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-20 20:42:38 +0900

    Add Movie Files

---
+++ src/Routes/Movie/MovieContainer.js
@@ -0,0 +1,50 @@
+import { movieApi } from "api";
+import React, { useEffect, useState } from "react";
+import MoviePresenter from "./MoviePresenter";
+
+const MovieContainer = () => {
+  const [nowPlaying, setNowPlaying] = useState(null);
+  const [popular, setPopular] = useState(null);
+  const [upcoming, setUpcoming] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getMovies = async () => {
+    try {
+      const {
+        data: { results: nowPlaying },
+      } = await movieApi.nowPlaying();
+      const {
+        data: { results: popular },
+      } = await movieApi.popular();
+      const {
+        data: { results: upcoming },
+      } = await movieApi.upcoming();
+      setNowPlaying(nowPlaying);
+      setPopular(popular);
+      setUpcoming(upcoming);
+    } catch (error) {
+      setError("Movie information cannot be loaded :(");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      <MoviePresenter
+        nowPlaying={nowPlaying}
+        popular={popular}
+        upcoming={upcoming}
+        loading={loading}
+        error={error}
+      />
+    </div>
+  );
+};
+
+export default MovieContainer;
