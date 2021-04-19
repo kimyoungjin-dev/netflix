2972998b83b18dc60fca7b53c5f960b41433ffb5
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-20 03:48:33 +0900

    Use HelmetProvider

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import AppRouter from "Components/Router";
 import GlobalStyles from "Components/GlobalStyles";
+import { HelmetProvider } from "react-helmet-async";
 
 const App = () => {
   return (
-    <>
+    <HelmetProvider>
       <AppRouter />
       <GlobalStyles />
-    </>
+    </HelmetProvider>
   );
 };
 
